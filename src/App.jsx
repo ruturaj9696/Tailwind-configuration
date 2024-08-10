eae55df00dc3b91327086f6a55ebb15adf231a8a
piyushd23 <piyush.deshmukh2304@gmail.com>
2024-08-10 23:18:57 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
-
+import Hero from './components/hero';
+import Navbar from './Navbar';
 const App = () => {
   return (
     <div>
       <div className='bg-red-400 m-3 p-2'>
         This is dummy file 
+        
       </div>
+      <Navbar/>
+      <Hero />
+
     </div>
   );
 }
